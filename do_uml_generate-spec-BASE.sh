90e2374a9dd145cd19ef32dc0b24e3a8b6273734
wolandscat <thomas.beale@openehr.org>
2017-10-07 09:30:49 +0100

    Update index page - remove BMM as separate UML file.

--- do_uml_generate-spec-BASE.sh
+++ do_uml_generate-spec-BASE.sh
@@ -1,4 +1,4 @@
 rm docs/UML/classes/*.*
 rm docs/UML/diagrams/*.*
+
 ./uml_generate.sh -i {base_release} -o docs/UML computable/UML/openEHR_UML-Base.mdzip
-./uml_generate.sh -i {base_release} -o docs/UML computable/UML/openEHR_UML-BMM.mdzip
