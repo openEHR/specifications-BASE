9b899d3ac84ece8dd4c4dafff40c70de0ac909ae
wolandscat <thomas.beale@openehr.org>
2019-10-16 11:51:46 -0300

    Update publication scripts.

--- do_uml_generate-spec-BASE.sh
+++ do_uml_generate-spec-BASE.sh
@@ -1,4 +1,4 @@
 rm docs/UML/classes/*.*
 rm docs/UML/diagrams/*.*
 
-./uml_generate.sh -i {base_release} -r BASE -o docs/UML computable/UML/openEHR_UML-Base.mdzip
+../specifications-AA_GLOBAL/bin/uml_generate.sh -i {base_release} -r BASE -o docs/UML computable/UML/openEHR_UML-Base.mdzip
