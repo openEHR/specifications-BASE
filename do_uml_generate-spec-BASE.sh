50b2a5dc5344fc50574caec9918ad97cd83f8118
wolandscat <thomas.beale@openehr.org>
2020-03-01 15:37:09 +0000

    Remove UML PNGs from UML extract generation.

--- do_uml_generate-spec-BASE.sh
+++ do_uml_generate-spec-BASE.sh
@@ -1,4 +1,4 @@
 rm docs/UML/classes/*.*
 rm docs/UML/diagrams/*.*
 
-../specifications-AA_GLOBAL/bin/uml_generate.sh -i {base_release} -r BASE -o docs/UML computable/UML/openEHR_UML-Base.mdzip
+../specifications-AA_GLOBAL/bin/uml_generate.sh -d svg -i {base_release} -r BASE -o docs/UML computable/UML/openEHR_UML-Base.mdzip
